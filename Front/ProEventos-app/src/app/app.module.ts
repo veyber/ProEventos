9e99a0ed587b609751c17011c3b7ff53eaf4a774
Veyber Lucas <veyberlucas@gmail.com>
2021-10-13 20:29:02 -0300

    Seção 7: Angular - Organizando rota, alerta e mais

--- Front/ProEventos-app/src/app/app.module.ts
+++ Front/ProEventos-app/src/app/app.module.ts
@@ -14,24 +14,32 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
-import { EventoService } from './services/evento.service';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { NavComponent } from './shared/nav/nav.component';
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+import { EventoService } from './services/evento.service';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
+    DateTimeFormatPipe,
     EventosComponent,
     PalestrantesComponent,
-    NavComponent,
-    DateTimeFormatPipe
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent,
+    TituloComponent
 
    ],
   imports: [
-    FormsModule,
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
--- Front/ProEventos-app/src/app/shared/titulo/titulo.component.spec.ts
+++ Front/ProEventos-app/src/app/shared/titulo/titulo.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { NavComponent } from './nav.component';
+import { TituloComponent } from './titulo.component';
 
-describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+describe('TituloComponent', () => {
+  let component: TituloComponent;
+  let fixture: ComponentFixture<TituloComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ TituloComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavComponent);
+    fixture = TestBed.createComponent(TituloComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
